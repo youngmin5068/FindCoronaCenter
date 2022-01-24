dd90006dfc315affb92c6ed6252e1999edad5b93
youngmin5068 <yougmin5068@naver.com>
2022-01-24 18:09:10 +0900

    Initial Commit

---
+++ FindCoronaCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindCoronaCenter
+//
+//  Created by 김영민 on 2022/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by 김영민 on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
